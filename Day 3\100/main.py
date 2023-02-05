1a3d38bb8dbdc0263ddc06ae47a4b194106d378b
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-05 16:43:40 +0000

    Update file

--- Day 3\100/main.py
+++ Day 3\100/main.py
@@ -3,11 +3,11 @@
 myName = input("Enter Name : ")
 myFood = input("Meal for the day : ")
 myPlant = input("Favorite edible plant to go with meal : ")
-myMethod = input("Cooking method :  ")
+myMethod = input("Cooking method : ")
 myType = input("How would you describe a burnt plant? ")
 myItem = input("Your choice of restaurantware? ")
 myTime = input("What time would you like your meal to be ready? ")
 print()
 print("Tonight's MENU:")
 #Print out a recipe page in concatenated format
-print(myName,"'s order for the day is: ", myMethod, myFood, "with", myType, myPlant, "on a", myItem )
+print("Hi", myName, "your order for the day is :", myMethod, myFood, "with", myType, myPlant, "on", myItem, "and the order should be ready at", myTime)
