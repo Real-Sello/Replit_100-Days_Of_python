12de5231c2ee0163d34508faf8a3d78e9c667841
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-05 13:47:33 +0000

    Initial commit

---
+++ Day 2\100/main.py
@@ -0,0 +1,23 @@
+#Getting to know you program
+
+print("Getting to know you!")
+
+myName = input("What is your name? : ")
+myFood = input ("What is your favorite food? : ")
+myMusic = input("What is your favorite music? : ")
+myLocation = input("Where do you live? : ")
+
+print("Your name is ")
+print(myName)
+print() 
+
+print("Your favorite food is ")
+print(myFood)
+print()
+print("Your favorite music is ")
+print (myMusic)
+print()
+print ("You're currently in")
+print (myLocation)
+print() 
+print ("You're awesome. Thanks for participating, have a great day!")
