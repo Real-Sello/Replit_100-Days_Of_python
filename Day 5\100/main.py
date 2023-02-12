b329c8ac8e1a3f7768c477e21699a46e42b627c5
Sello Prince Moneatse <spmoneatse@gmail.com>
2023-02-12 13:17:51 +0200

    Update Day 5

---
+++ Day 5\100/main.py
@@ -0,0 +1,39 @@
+#Which character are you generator
+#identify type of user
+user_type = input("Do you Like Marvel Movies? Y/N: ")
+if user_type == "Y":
+  print("Great, Which character are you from 'Avengers?'")
+  print()
+  print("Answer these questions and let's find out.")
+  print()
+  print("Please use Y or N for yes and no.")
+
+  userType = input("Do you like smashing things?: ")
+  if userType == "Y":
+    print("GRRRRRR. You are the Hulk")
+  else:
+    print("Hulk smash you, You are tiny and not the Hulk")
+  print()
+  userType1 = input("Are you worthy of Mjolnir?: ")
+  if userType1 == "Y":
+    print("AAH Thor Odinson, feast god of thunder")
+  else:
+    print("How dare you attempt to wield Mjolnir? You are not Thor")
+  print()
+
+  userType3 = input("Have you been in the quantum dimension?: ")
+  if  userType3 == "Y":
+    print("Hello Antman, It's nice to see you")
+  else:
+    print("NO, You're not Antman")
+  print()
+
+  userType4 = input("Do you love tech?: ")
+  if userType4 == "Y":
+    print("Welcome Mr Stark, You're definitely Ironman")
+  else:
+    print("You are not Ironman, are you one of Kilmonger's minions?")
+  print()
+else:
+  print("I guess you are not like anyone from 'Avengers.'")
+print("Thanks for participating, follow me @Real-Sello")
